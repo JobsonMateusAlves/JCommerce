6cc77038d66c0df7ecdb49e92279bcf6ba3b3a78
Jobson Mateus <jobson@klever.io>
2023-05-19 01:29:40 -0300

    Add Products ViewController

---
+++ Presentation/Sources/Presentation/Products/ProductsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProductsViewController.swift
+//  
+//
+//  Created by Jobson Mateus on 19/05/23.
+//
+
+import UIKit
+
+class ProductsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
