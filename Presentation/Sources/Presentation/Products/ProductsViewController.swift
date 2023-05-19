5eef9ea280e004935d2f6c4f5e67241205720d92
Jobson Mateus <jobson@klever.io>
2023-05-19 01:39:08 -0300

    Remove Storyboard and set RootViewController programmatically

--- Presentation/Sources/Presentation/Products/ProductsViewController.swift
+++ Presentation/Sources/Presentation/Products/ProductsViewController.swift
@@ -7,10 +7,18 @@
 
 import UIKit
 
-class ProductsViewController: UIViewController {
+public class ProductsViewController: UIViewController {
+    
+    // MARK: Inits
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 }
